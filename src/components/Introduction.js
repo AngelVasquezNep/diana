ca6d6285fcc0324d1b340ef92c5d94d499c93fe5
Angelito <angel.vasquez.unam@gmail.com>
2020-08-17 00:03:37 -0500

    Fix typo

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Introduction = () => (
   <section>
-    <h1 style={{ fontWeight: 'normal' }}>Hola, soy la psicólogia Diana Solís.</h1>
+    <h1 style={{ fontWeight: 'normal' }}>Hola, soy la psicóloga Diana Solís.</h1>
     <p>
       Soy una profesional de la psicólogia con muchos años de experiencia, me he
       especializado en diversas áreas de la salud y junto con un equipo de expertos contamos
